131c1c1cfcab5bdac3a059832c48da957abcdc8d
Manfred Quasten <manfred.quasten@arcor.de>
2018-03-01 20:43:34 +0100

    get the party started

---
+++ iot.domain/src/main/java/de/mq/iot/domain/state/BooleanStateImpl.java
@@ -0,0 +1,55 @@
+package de.mq.iot.domain.state;
+
+import java.time.ZonedDateTime;
+
+class BooleanStateImpl implements State<Boolean>{
+
+	/*
+	 * (non-Javadoc)
+	 * @see de.mq.iot.domain.state.State#id()
+	 */
+	@Override
+	public final long id() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see de.mq.iot.domain.state.State#name()
+	 */
+	@Override
+	public final String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see de.mq.iot.domain.state.State#value()
+	 */
+	@Override
+	public final  Boolean value() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see de.mq.iot.domain.state.State#lastupdate()
+	 */
+	@Override
+	public final ZonedDateTime lastupdate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see de.mq.iot.domain.state.State#assign(java.lang.Object)
+	 */
+	@Override
+	public void assign(Boolean value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ iot.domain/src/main/java/de/mq/iot/domain/state/State.java
@@ -0,0 +1,43 @@
+package de.mq.iot.domain.state;
+
+import java.time.ZonedDateTime;
+/**
+ * One state of a finite state machine.
+ * @author Admin
+ *
+ * @param <T> type of the state, Boolean, Long, String etc.
+ */
+public interface State<T> {
+	
+	/**
+	 * internal id ccu2
+	 * Request Parameter ise_id.
+	 * @return the ccu2 id 
+	 */
+	long id();
+	
+	/**
+	 * The name of the state in ccu2.
+	 * @return
+	 */
+	String name();
+	
+	/**
+	 * The value of the state, Boolean, Long, String etc.
+	 * @return the state value
+	 */
+	T value();
+	
+	/**
+	 * Last changed date. 
+	 * @return time last changed
+	 */
+	ZonedDateTime lastupdate();
+	
+	/**
+	 * Change the state value
+	 * @param value the value to that the state should be changed
+	 */
+	void assign(T value);
+
+}
