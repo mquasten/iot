abbcf0a28ec1c044ec0f5742fcbac2df2c0dbf56
Manfred Quasten <manfred.quasten@arcor.de>
2018-03-03 22:08:53 +0100

    test

---
+++ iot.application/src/test/java/de/mq/iot/state/StateRepositoryTest.java
@@ -0,0 +1,25 @@
+package de.mq.iot.state;
+
+import org.junit.jupiter.api.Test;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+
+
+class StateRepositoryTest {
+	
+	
+	
+	
+	
+	private final HomematicXmlApiStateRepositoryImpl stateRepository = new HomematicXmlApiStateRepositoryImpl();
+	
+	@Test
+	void findStates() throws UnirestException {
+		stateRepository.findStates();
+	
+	}
+
+}
+
+
