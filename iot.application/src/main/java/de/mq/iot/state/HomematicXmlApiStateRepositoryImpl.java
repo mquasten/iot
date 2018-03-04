36db7cfc315ae7fc99c4e0383bcf14ff91d3090b
binaris <binaris@DESKTOP-MBCG59L>
2018-03-04 22:23:08 +0100

    projekt setup

--- iot.application/src/main/java/de/mq/iot/state/HomematicXmlApiStateRepositoryImpl.java
+++ iot.application/src/main/java/de/mq/iot/state/HomematicXmlApiStateRepositoryImpl.java
@@ -1,80 +1,90 @@
 package de.mq.iot.state;
 
+import java.io.IOException;
+import java.io.StringBufferInputStream;
 import java.util.Collection;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.reactive.function.client.WebClient;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import de.mq.iot.domain.state.State;
 
+@Repository
+abstract class HomematicXmlApiStateRepositoryImpl {
 
-class HomematicXmlApiStateRepositoryImpl {
-	
-	
-	
-	
-	Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-	
-	
-		
+	Collection<State<?>> findStates() throws ParserConfigurationException, SAXException, IOException {
 
-	Collection<State<?>> findStates() throws UnirestException {
-		marshaller.setPackagesToScan("de.mq.iot.state");
-		
-		marshaller.setMappedClass(HomematicState.class);
-		
-		//HttpResponse<String> response = Unirest.get("http://{host}/addons/xmlapi/sysvarlist.cgi").routeParam("host", "192.168.2.104").asString();
-		
-		
-		//System.out.println(response.getBody());
-		
-		
-		
-		ResponseEntity<String>  res = WebClient.builder().build().get().uri("http://{host}/addons/xmlapi/sysvarlist.cgi",  "192.168.2.104").exchange().block().toEntity(String.class).block();
-		
-		if( res.getStatusCode().is4xxClientError() || res.getStatusCode().is5xxServerError() ) {
-			
+		// http://mq65.ddns.net:2000/addons/xmlapi/sysvarlist.cgi{}
+
+		final ResponseEntity<String> res = webClientBuilder().build().get()
+				.uri("http://{host}:{port}/addons/xmlapi/sysvarlist.cgi", "mq65.ddns.net", 2000).exchange().block()
+				.toEntity(String.class).block();
+
+		if (res.getStatusCode().is4xxClientError() || res.getStatusCode().is5xxServerError()) {
+
 			throw new HttpStatusCodeException(res.getStatusCode(), res.getStatusCode().getReasonPhrase()) {
 
 				private static final long serialVersionUID = 1L;
 			};
-			
+
 		}
 		System.out.println(res.getBody());
-		
-		
-		//final String response = WebClient.create("http://192.168.2.104/addons/xmlapi/sysvarlist.cgix"  ).get().retrieve().bodyToMono(String.class).block();
-		
-		
-		
-/*		
-		final XPathOperations  xPathOperations  = new Jaxp13XPathTemplate();
-		xPathOperations.evaluate("/systemVariables/systemVariable", new StreamSource(new StringReader(response)), (node, num) -> {
-			
-			
-			String var = nodeToString(node);
-			
-			System.out.println(var);
-			HomematicState result  = (HomematicState) marshaller.unmarshal(new StreamSource(new StringReader(var)));
-			
-		
-			System.out.println(result.getId()+ "," + result.getName()+ "," + result.getMin());
-			return null;
-		});
-		return null;
-		
-		
-		*/
-		return null;
-		
-		
-	}
-	
-	
 
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+
+		Document doc = builder.parse(new StringBufferInputStream(res.getBody()));
+		Element root = doc.getDocumentElement();
+
+		NodeList list = root.getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) {
+			NamedNodeMap attributes = list.item(i).getAttributes();
+			for (int j = 0; j < attributes.getLength(); j++) {
+				final Node node = attributes.item(j);
+				System.out.println(node.getNodeName() + ":" + node.getNodeValue());
+			}
+			System.out.println("************************************");
+		}
+
+		// final String response =
+		// WebClient.create("http://192.168.2.104/addons/xmlapi/sysvarlist.cgix"
+		// ).get().retrieve().bodyToMono(String.class).block();
+
+		/*
+		 * final XPathOperations xPathOperations = new Jaxp13XPathTemplate();
+		 * xPathOperations.evaluate("/systemVariables/systemVariable", new
+		 * StreamSource(new StringReader(response)), (node, num) -> {
+		 * 
+		 * 
+		 * String var = nodeToString(node);
+		 * 
+		 * System.out.println(var); HomematicState result = (HomematicState)
+		 * marshaller.unmarshal(new StreamSource(new StringReader(var)));
+		 * 
+		 * 
+		 * System.out.println(result.getId()+ "," + result.getName()+ "," +
+		 * result.getMin()); return null; }); return null;
+		 * 
+		 * 
+		 */
+		return null;
+
+	}
+
+	@Lookup
+	abstract Builder webClientBuilder();
 
 }
--- iot.application/src/test/java/de/mq/iot/state/StateRepositoryTest.java
+++ iot.application/src/test/java/de/mq/iot/state/StateRepositoryTest.java
@@ -1,26 +1,47 @@
 package de.mq.iot.state;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.xml.sax.SAXException;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
+@ExtendWith(SpringExtension.class)
 
-
-
+@ContextConfiguration(classes = { TestConfiguration.class })
 class StateRepositoryTest {
-	
-	
-	
-	
-	
-	private final HomematicXmlApiStateRepositoryImpl stateRepository = new HomematicXmlApiStateRepositoryImpl();
-	
+
+	@Autowired
+	private HomematicXmlApiStateRepositoryImpl stateRepository;
+
 	@Test
-	void findStates() throws UnirestException {
-	
+	@Disabled
+	void findStates() throws ParserConfigurationException, SAXException, IOException {
+		System.out.println(stateRepository);
 		stateRepository.findStates();
-	
+
 	}
 
 }
 
+@Configuration
+@ComponentScan(basePackages = "de.mq.iot.state")
+class TestConfiguration {
+	@Bean
+	@Scope(scopeName = "prototype")
+	WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
+	}
 
+}
