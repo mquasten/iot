ee09aa2efb8360f8fec96345f2ef37f4fcc289ba
binaris <binaris@DESKTOP-MBCG59L>
2018-03-05 22:12:45 +0100

    cean StateRepository

--- iot.application/src/main/java/de/mq/iot/state/HomematicXmlApiStateRepositoryImpl.java
+++ iot.application/src/main/java/de/mq/iot/state/HomematicXmlApiStateRepositoryImpl.java
@@ -1,38 +1,56 @@
 package de.mq.iot.state;
 
-import java.io.IOException;
-import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.util.AbstractMap;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 
 import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.reactive.function.client.WebClient.Builder;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import de.mq.iot.domain.state.State;
+import org.xml.sax.InputSource;
 
 @Repository
 abstract class HomematicXmlApiStateRepositoryImpl {
 
-	Collection<State<?>> findStates() throws ParserConfigurationException, SAXException, IOException {
+	Collection<Map<String, String>> findStates() {
 
 		// http://mq65.ddns.net:2000/addons/xmlapi/sysvarlist.cgi{}
 
 		final ResponseEntity<String> res = webClientBuilder().build().get()
 				.uri("http://{host}:{port}/addons/xmlapi/sysvarlist.cgi", "mq65.ddns.net", 2000).exchange().block()
 				.toEntity(String.class).block();
+		httpStatusGuard(res);
 
+		final NodeList nodes = evaluate(res);
+
+		return IntStream.range(0, nodes.getLength()).mapToObj(i -> attributesToMap(nodes.item(i).getAttributes()))
+				.collect(Collectors.toList());
+
+	}
+
+	private NodeList evaluate(final ResponseEntity<String> res) {
+		try {
+			return (NodeList) xpath().evaluate("/systemVariables/systemVariable",
+					new InputSource(new StringReader(res.getBody())), XPathConstants.NODESET);
+		} catch (XPathExpressionException ex) {
+			throw new IllegalStateException("Unable to evalualte xpath expression", ex);
+
+		}
+	}
+
+	private void httpStatusGuard(final ResponseEntity<String> res) {
 		if (res.getStatusCode().is4xxClientError() || res.getStatusCode().is5xxServerError()) {
 
 			throw new HttpStatusCodeException(res.getStatusCode(), res.getStatusCode().getReasonPhrase()) {
@@ -41,50 +59,20 @@ abstract class HomematicXmlApiStateRepositoryImpl {
 			};
 
 		}
-		System.out.println(res.getBody());
+	}
 
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-
-		Document doc = builder.parse(new StringBufferInputStream(res.getBody()));
-		Element root = doc.getDocumentElement();
-
-		NodeList list = root.getChildNodes();
-		for (int i = 0; i < list.getLength(); i++) {
-			NamedNodeMap attributes = list.item(i).getAttributes();
-			for (int j = 0; j < attributes.getLength(); j++) {
-				final Node node = attributes.item(j);
-				System.out.println(node.getNodeName() + ":" + node.getNodeValue());
-			}
-			System.out.println("************************************");
-		}
-
-		// final String response =
-		// WebClient.create("http://192.168.2.104/addons/xmlapi/sysvarlist.cgix"
-		// ).get().retrieve().bodyToMono(String.class).block();
-
-		/*
-		 * final XPathOperations xPathOperations = new Jaxp13XPathTemplate();
-		 * xPathOperations.evaluate("/systemVariables/systemVariable", new
-		 * StreamSource(new StringReader(response)), (node, num) -> {
-		 * 
-		 * 
-		 * String var = nodeToString(node);
-		 * 
-		 * System.out.println(var); HomematicState result = (HomematicState)
-		 * marshaller.unmarshal(new StreamSource(new StringReader(var)));
-		 * 
-		 * 
-		 * System.out.println(result.getId()+ "," + result.getName()+ "," +
-		 * result.getMin()); return null; }); return null;
-		 * 
-		 * 
-		 */
-		return null;
-
+	private Map<String, String> attributesToMap(final NamedNodeMap nodeMap) {
+		final Map<String, String> attributes = IntStream.range(0, nodeMap.getLength())
+				.mapToObj(j -> new AbstractMap.SimpleImmutableEntry<String, String>(nodeMap.item(j).getNodeName(),
+						nodeMap.item(j).getNodeValue()))
+				.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+		return attributes;
 	}
 
 	@Lookup
 	abstract Builder webClientBuilder();
+
+	@Lookup
+	abstract XPath xpath();
 
 }
--- iot.application/src/test/java/de/mq/iot/state/StateRepositoryTest.java
+++ iot.application/src/test/java/de/mq/iot/state/StateRepositoryTest.java
@@ -1,10 +1,21 @@
 package de.mq.iot.state;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.xml.parsers.ParserConfigurationException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Disabled;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +26,43 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.xml.sax.SAXException;
 
 @ExtendWith(SpringExtension.class)
-
 @ContextConfiguration(classes = { TestConfiguration.class })
 class StateRepositoryTest {
 
+	private static final String ATTRIBUTE_VALUE = "value";
+	private static final String TYPE_BOOLEAN = "2";
+	private static final String ATTRIBUTE_ID = "ise_id";
+	private static final String ATTRIBUTE_TYPE = "type";
+	private static final String ATTRIBUTE_NAME = "name";
+	private static final String NAME_WORKINGDAY = "Workingday";
 	@Autowired
 	private HomematicXmlApiStateRepositoryImpl stateRepository;
 
 	@Test
-	@Disabled
-	void findStates() throws ParserConfigurationException, SAXException, IOException {
-		System.out.println(stateRepository);
-		stateRepository.findStates();
 
+	void findStates() {
+		final Collection<Map<String, String>> results = stateRepository.findStates();
+		assertFalse(results.isEmpty());
+
+		final Map<String, String> result = singleUniqueResult(
+				results.stream().filter(entry -> entry.get(ATTRIBUTE_NAME).equalsIgnoreCase(NAME_WORKINGDAY))
+						.collect(Collectors.toList()));
+
+		assertEquals(NAME_WORKINGDAY, result.get(ATTRIBUTE_NAME));
+		assertEquals(TYPE_BOOLEAN, result.get(ATTRIBUTE_TYPE));
+		assertNotNull(result.get(ATTRIBUTE_ID));
+		Integer.parseInt(result.get(ATTRIBUTE_ID));
+		assertTrue(Arrays.asList("true", "false").contains(result.get(ATTRIBUTE_VALUE)));
+		assertTrue(2017 < LocalDateTime
+				.ofInstant(Instant.ofEpochSecond(Long.parseLong(result.get("timestamp"))), ZoneOffset.UTC).getYear());
 	}
 
+	<T> T singleUniqueResult(final Collection<T> values) {
+		assertEquals(1, values.size());
+		return values.stream().findAny().orElseThrow(() -> new IllegalStateException("Result is mandatory."));
+	}
 }
 
 @Configuration
@@ -42,6 +72,12 @@ class TestConfiguration {
 	@Scope(scopeName = "prototype")
 	WebClient.Builder webClientBuilder() {
 		return WebClient.builder();
+	}
+
+	@Bean
+	@Scope(scopeName = "prototype")
+	XPath xpath() {
+		return XPathFactory.newInstance().newXPath();
 	}
 
 }
