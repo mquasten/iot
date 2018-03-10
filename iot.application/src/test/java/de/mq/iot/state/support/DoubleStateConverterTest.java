56bb45fce3f86b30cb6be7cf19ef5bc18f89828a
Manfred Quasten <manfred.quasten@arcor.de>
2018-03-10 20:18:18 +0100

    DoubleState

--- iot.application/src/test/java/de/mq/iot/state/support/DoubleStateConverterTest.java
+++ iot.application/src/test/java/de/mq/iot/state/support/DoubleStateConverterTest.java
@@ -12,47 +12,44 @@ import java.util.TimeZone;
 import org.junit.jupiter.api.Test;
 
 class DoubleStateConverterTest {
-	
+
 	private static final Double VALUE = Double.valueOf(47.11);
 
 	private final AbstractStateConverter<Double> converter = new DoubleStateConverter();
-	
+
 	private static final String ID = "4711";
 	private static final String NAME = "DoubleValue";
 	private static final String TIMESTAMP = "" + new Date().getTime() / 1000;
-	
-	
+
 	@Test
 	void key() {
 		assertEquals("4", converter.key());
 	}
-	
+
 	@Test
 	void target() {
 		assertEquals(DoubleStateImpl.class, converter.target());
-		
+
 		final State<Double> state = converter.convert(valuesMap());
-		assertEquals(Long.valueOf(ID),Long.valueOf( state.id()));
-		assertEquals(NAME,state.name());
-		assertEquals(VALUE,state.value());
+		assertEquals(Long.valueOf(ID), Long.valueOf(state.id()));
+		assertEquals(NAME, state.name());
+		assertEquals(VALUE, state.value());
 		assertEquals(expectedTime(), state.lastupdate());
-		
+
 	}
 
-	
-	
 	private Map<String, String> valuesMap() {
-		final Map<String,String> valuesMap = new HashMap<>();
+		final Map<String, String> valuesMap = new HashMap<>();
 		valuesMap.put(StateConverter.KEY_ID, ID);
 		valuesMap.put(StateConverter.KEY_NAME, NAME);
 		valuesMap.put(StateConverter.KEY_TIMESTAMP, TIMESTAMP);
-		valuesMap.put(StateConverter.KEY_VALUE, ""+ VALUE);
+		valuesMap.put(StateConverter.KEY_VALUE, "" + VALUE);
 		valuesMap.put(StateConverter.KEY_TYPE, "4");
 		return valuesMap;
 	}
-	
+
 	private LocalDateTime expectedTime() {
-		return LocalDateTime.ofInstant(Instant.ofEpochMilli(1000* Long.valueOf(TIMESTAMP)), TimeZone.getDefault().toZoneId());
+		return LocalDateTime.ofInstant(Instant.ofEpochMilli(1000 * Long.valueOf(TIMESTAMP)), TimeZone.getDefault().toZoneId());
 	}
-	
+
 }
