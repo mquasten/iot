8361b35470cddb7b2f864366b2529f009232011f
Manfred Quasten <manfred.quasten@arcor.de>
2018-03-17 12:38:01 +0100

    add StringState

--- iot.domain/src/main/java/de/mq/iot/state/support/StringStateImpl.java
+++ iot.domain/src/main/java/de/mq/iot/state/support/StringStateImpl.java
@@ -2,27 +2,33 @@ package de.mq.iot.state.support;
 
 import java.time.LocalDateTime;
 
-public class StringStateImpl extends AbstractState<String> {
+class StringStateImpl extends AbstractState<String> {
 
+	static final String NULL_VALUE_CCU = "???";
+
+	static final String DEFAULT_VALUE = "";
+
+	private String value=DEFAULT_VALUE;
+	
 	StringStateImpl(final long id, final String name, final LocalDateTime lastupdate) {
 		super(id, name, lastupdate);
 		assign(value -> value != null ? value.split("[\\s]").length == 1 : true );
 	}
 
-	private String value;
+	
 	
 	@Override
 	public String value() {
-		return value == null ? "" : value.replaceFirst("^[?]{3, 3}$","");
+		return value;
 		
 	}
 
 	@Override
 	public void assign(final String value) {
 		if( ! validate(value)) {
-			throw new IllegalArgumentException("String should not containo whitespaces.");
+			throw new IllegalArgumentException("String should not contain whitespaces.");
 		}
-		
+		this.value =  value == null ? DEFAULT_VALUE : value.equals(NULL_VALUE_CCU) ? DEFAULT_VALUE : value; 
 	}
 
 }
---
+++ iot.domain/src/test/java/de/mq/iot/state/support/StringStateTest.java
@@ -0,0 +1,65 @@
+package de.mq.iot.state.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+class StringStateTest {
+	
+	private static final String VALUE_INVALID = "Kylie\tis\tnice";
+	private static final String VALUE = "KylieIsNiceAnd...";
+	private static final String NAME = "StringState";
+	private static final long ID = 19680528L;
+	
+	private final State<String> state = new StringStateImpl(ID, NAME, LocalDateTime.now());
+	
+	
+	@Test
+	final void id() {
+		assertEquals(ID, state.id());
+	}
+	
+	@Test
+	final void name() {
+		assertEquals(NAME, state.name());
+	}
+	
+	@Test
+	final void lastUpdate() throws InterruptedException {
+		assertTrue(Math.abs(Duration.between(LocalDateTime.now(), state.lastupdate()).toMillis()) < 10);
+	}
+	
+	@Test
+	final void value() {
+		assertEquals(StringStateImpl.DEFAULT_VALUE, state.value());
+
+		state.assign(VALUE);
+		assertEquals(VALUE, state.value());
+		
+		state.assign(null);
+		assertEquals(StringStateImpl.DEFAULT_VALUE, state.value());
+		
+		state.assign(StringStateImpl.NULL_VALUE_CCU);
+		assertEquals(StringStateImpl.DEFAULT_VALUE, state.value());
+	}
+	
+	@Test
+	final void  validate() {
+		assertTrue(state.validate(VALUE));
+		assertFalse(state.validate(VALUE_INVALID));
+		assertFalse(state.validate(VALUE_INVALID.replace('\t', ' ')));
+		assertTrue(state.validate(VALUE_INVALID.replace('\t', '_')));
+	}
+	
+	@Test
+	final void assignInValid() {
+		assertThrows(IllegalArgumentException.class, () -> state.assign(VALUE_INVALID));
+	}
+
+}
