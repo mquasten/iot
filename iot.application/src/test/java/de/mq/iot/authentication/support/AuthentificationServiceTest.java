2c6db4614b312b8319a56a7e02826acaed633585
Manfred Quasten <manfred.quasten@arcor.de>
2018-05-14 21:02:07 +0200

    AuthentificationService test

---
+++ iot.application/src/test/java/de/mq/iot/authentication/support/AuthentificationServiceTest.java
@@ -0,0 +1,39 @@
+package de.mq.iot.authentication.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import de.mq.iot.authentication.Authentication;
+import reactor.core.publisher.Mono;
+
+public class AuthentificationServiceTest {
+	
+	private static final String USER = "kminogue";
+
+	private static final int TIMEOUT = 500;
+
+	private AuthenticationRepository authenticationRepository = Mockito.mock(AuthenticationRepository.class);
+	
+	private AuthentificationServiceImpl  authentificationService = new AuthentificationServiceImpl(authenticationRepository, TIMEOUT);
+	
+	private Authentication authentication = Mockito.mock(Authentication.class);
+	
+	@SuppressWarnings("unchecked")
+	private Mono<Authentication> mono = Mockito.mock(Mono.class);
+	
+	@Test
+	void authentification() {
+		
+		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
+		Mockito.doReturn(authentication).when(mono).block(Duration.ofMillis(TIMEOUT));
+		assertEquals(Optional.of(authentication), authentificationService.authentification(USER));
+	}
+	
+	
+
+}
