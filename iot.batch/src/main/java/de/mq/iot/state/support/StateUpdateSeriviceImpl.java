c8df1587c7d08ab892f58114659dac96efe0af3a
Manfred Quasten <manfred.quasten@arcor.de>
2018-06-21 21:24:37 +0200

    time WINTER/SUMMER

--- iot.batch/src/main/java/de/mq/iot/state/support/StateUpdateSeriviceImpl.java
+++ iot.batch/src/main/java/de/mq/iot/state/support/StateUpdateSeriviceImpl.java
@@ -1,12 +1,13 @@
 package de.mq.iot.state.support;
 
-
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,60 +21,84 @@ import de.mq.iot.state.StateUpdateService;
 
 @Service
 public class StateUpdateSeriviceImpl implements StateUpdateService {
-
+	static final String SUMMER = "SUMMER";
+	static final String WINTER = "WINTER";
 	static final String WORKINGDAY_STATE_NAME = "Workingday";
 	private final SpecialdayService specialdayService;
-	private final StateService  stateService;
-	
+	private final StateService stateService;
+
 	@Autowired
-	StateUpdateSeriviceImpl(final SpecialdayService specialdayService,final StateService  stateService) {
-		
+	StateUpdateSeriviceImpl(final SpecialdayService specialdayService, final StateService stateService) {
+
 		this.specialdayService = specialdayService;
-		this.stateService=stateService;
+		this.stateService = stateService;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.mq.iot.state.support.StateUpdateService#update(int)
 	 */
 	@Override
-	@Commands(commands= {@Command(arguments = { "d" }, name = "updateWorkingday"), @Command(arguments = { "d" }, name = "updateCalendar" , order=1)})
+	@Commands(commands = { @Command(arguments = { "d" }, name = "updateWorkingday"), @Command(arguments = { "d" }, name = "updateCalendar", order = 1) })
 	public void updateWorkingday(final int offsetDays) {
 		final LocalDate localDate = LocalDate.now().plusDays(offsetDays);
-		Assert.isTrue(offsetDays >=0 , "Offset days should be greather or equals 0." );
-		
+		Assert.isTrue(offsetDays >= 0, "Offset days should be greather or equals 0.");
+
 		@SuppressWarnings("unchecked")
 		final State<Boolean> workingDayState = (State<Boolean>) stateService.states().stream().filter(state -> state.name().equals(WORKINGDAY_STATE_NAME)).findAny().orElseThrow(() -> new IllegalStateException("Workingday State expected."));
 		final boolean expectedWorkingDayStateValue = isWorkingsday(localDate);
-		if( ! workingDayState.value().equals(expectedWorkingDayStateValue)) {
+		if (!workingDayState.value().equals(expectedWorkingDayStateValue)) {
 			System.out.println("update needed ...");
 			workingDayState.assign(expectedWorkingDayStateValue);
 			stateService.update(workingDayState);
-			
-			System.out.println("update workingday to:" +expectedWorkingDayStateValue );
+
+			System.out.println("update workingday to:" + expectedWorkingDayStateValue);
 		}
-		
-		
-		
-	}
-	
-	@Commands(commands= {@Command(arguments = { "d" }, name = "updateTime"), @Command(arguments = { "d" }, name = "updateCalendar") })
-	public void updateTime(final int offsetDays) {
-		
+
 	}
 
-	boolean  isWorkingsday(final LocalDate date) {
-		if ( Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(date.getDayOfWeek())) {
-		   return false;
-		}
-		
-		final Collection<LocalDate> specialdates = specialdayService.specialdays(Year.from(date)).stream().map(specialday -> specialday.date(date.getYear())).collect(Collectors.toSet());
-		
-		if(specialdates.contains(date)) {
-			
+	@Commands(commands = { @Command(arguments = { "d" }, name = "updateTime"), @Command(arguments = { "d" }, name = "updateCalendar") })
+	public void updateTime(final int offsetDays) {
+
+	}
+
+	boolean isWorkingsday(final LocalDate date) {
+		if (Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(date.getDayOfWeek())) {
 			return false;
 		}
-	    
+
+		final Collection<LocalDate> specialdates = specialdayService.specialdays(Year.from(date)).stream().map(specialday -> specialday.date(date.getYear())).collect(Collectors.toSet());
+
+		if (specialdates.contains(date)) {
+
+			return false;
+		}
+
 		return true;
 	}
-	
+
+	String time(final LocalDate date) {
+
+		final LocalDate startSummerTime = lastSundayInMonth(Year.of(date.getYear()), Month.MARCH);
+
+		final LocalDate startWinterTime = lastSundayInMonth(Year.of(date.getYear()), Month.OCTOBER);
+
+		if (afterEquals(date, startSummerTime) && date.isBefore(startWinterTime)) {
+			return SUMMER;
+		}
+
+		return WINTER;
+	}
+
+	private boolean afterEquals(final LocalDate date, final LocalDate startSummerTime) {
+		return date.isAfter(startSummerTime) || date.isEqual(startSummerTime);
+	}
+
+	LocalDate lastSundayInMonth(final Year year, final Month month) {
+		final LocalDate start = LocalDate.of(year.getValue(), Month.of(month.getValue() + 1), 1);
+		return IntStream.range(1, 8).mapToObj(i -> start.minusDays(i)).filter(date -> date.getDayOfWeek().equals(DayOfWeek.SUNDAY)).findFirst().orElseThrow(() -> new IllegalArgumentException("Date is required"));
+
+	}
+
 }
--- iot.batch/src/test/java/de/mq/iot/state/support/StateUpdateSeriviceTest.java
+++ iot.batch/src/test/java/de/mq/iot/state/support/StateUpdateSeriviceTest.java
@@ -1,13 +1,18 @@
 package de.mq.iot.state.support;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Year;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -20,74 +25,71 @@ import de.mq.iot.state.StateService;
 import de.mq.iot.state.StateUpdateService;
 
 public class StateUpdateSeriviceTest {
-	
-	
+
 	private final SpecialdayService specialdayService = Mockito.mock(SpecialdayService.class);
-	private final StateService  stateService = Mockito.mock(StateService.class);
-	
-	
+	private final StateService stateService = Mockito.mock(StateService.class);
+
 	private final StateUpdateService stateUpdateService = new StateUpdateSeriviceImpl(specialdayService, stateService);
-	
+
 	@SuppressWarnings("unchecked")
 	private final State<Boolean> state = Mockito.mock(State.class);
-	
+
 	@BeforeEach
 	void setup() {
-	
-	;
+
+		;
 		Mockito.doReturn(StateUpdateSeriviceImpl.WORKINGDAY_STATE_NAME).when(state).name();
 		Mockito.doReturn(Boolean.FALSE).when(state).value();
 		Mockito.doReturn(Arrays.asList(state)).when(stateService).states();
 	}
-	
+
 	@Test
 	void updateWorkingday() {
 		final LocalDate localdate = LocalDate.now();
 		final Integer offset = workingDayOffset(localdate);
-		
-		
-		stateUpdateService.updateWorkingday(offset);
-		 Mockito.verify(state).assign(Boolean.TRUE);
 
-		 Mockito.verify(stateService).update(state);
+		stateUpdateService.updateWorkingday(offset);
+		Mockito.verify(state).assign(Boolean.TRUE);
+
+		Mockito.verify(stateService).update(state);
 
 	}
 
 	private int workingDayOffset(final LocalDate localdate) {
-		return IntStream.range(0, 7).filter(i -> ! (localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SATURDAY)||localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SUNDAY))).findFirst().getAsInt();
+		return IntStream.range(0, 7).filter(i -> !(localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SATURDAY) || localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SUNDAY))).findFirst().getAsInt();
 	}
-	
+
 	@Test
 	void updateWorkingdayNothingTodo() {
 		Mockito.doReturn(Boolean.TRUE).when(state).value();
 		final LocalDate localdate = LocalDate.now();
 		final Integer offset = workingDayOffset(localdate);
-		
-		
-		stateUpdateService.updateWorkingday(offset);
-		Mockito.verify(state,Mockito.never()).assign(Boolean.TRUE);
 
-		 Mockito.verify(stateService,Mockito.never()).update(state);
+		stateUpdateService.updateWorkingday(offset);
+		Mockito.verify(state, Mockito.never()).assign(Boolean.TRUE);
+
+		Mockito.verify(stateService, Mockito.never()).update(state);
 
 	}
-	
+
 	@Test
 	void updateWorkingdayInvalidOffset() {
 		assertThrows(IllegalArgumentException.class, () -> stateUpdateService.updateWorkingday(-1));
 	}
-	
+
 	@Test
 	void updateWorkingdayStateMissing() {
 		Mockito.doReturn("???").when(state).name();
 		Mockito.doReturn(Arrays.asList(state)).when(stateService).states();
-		
+
 		assertThrows(IllegalStateException.class, () -> stateUpdateService.updateWorkingday(0));
 	}
+
 	@Test
 	void isWorkingsdaySpecialDay() {
 		final LocalDate testDate = prepareSpecialDays();
-		
-		assertFalse(((StateUpdateSeriviceImpl)stateUpdateService).isWorkingsday(testDate));
+
+		assertFalse(((StateUpdateSeriviceImpl) stateUpdateService).isWorkingsday(testDate));
 	}
 
 	private LocalDate prepareSpecialDays() {
@@ -95,27 +97,45 @@ public class StateUpdateSeriviceTest {
 		final Integer offset = workingDayOffset(localdate);
 		final LocalDate testDate = localdate.plusDays(offset);
 		final Specialday specialday = Mockito.mock(Specialday.class);
-		
+
 		Mockito.doReturn(testDate).when(specialday).date(testDate.getYear());
 		Mockito.doReturn(Arrays.asList(specialday)).when(specialdayService).specialdays(Year.of(testDate.getYear()));
 		return testDate;
 	}
-	
+
 	private int workingWeekEndOffset(final LocalDate localdate) {
-		return IntStream.range(0, 7).filter(i -> localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SATURDAY)||localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SUNDAY)).findFirst().getAsInt();
+		return IntStream.range(0, 7).filter(i -> localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SATURDAY) || localdate.plusDays(i).getDayOfWeek().equals(DayOfWeek.SUNDAY)).findFirst().getAsInt();
 	}
-	
+
 	@Test
 	void isWorkingsdayWeekend() {
 		final LocalDate testDate = LocalDate.now().plusDays(workingWeekEndOffset(LocalDate.now()));
-		
-		assertFalse(((StateUpdateSeriviceImpl)stateUpdateService).isWorkingsday(testDate));
+
+		assertFalse(((StateUpdateSeriviceImpl) stateUpdateService).isWorkingsday(testDate));
 	}
-	
+
 	@Test
 	void isWorkingsday() {
 		final LocalDate testDate = LocalDate.now().plusDays(workingDayOffset(LocalDate.now()));
-		assertTrue(((StateUpdateSeriviceImpl)stateUpdateService).isWorkingsday(testDate));
+		assertTrue(((StateUpdateSeriviceImpl) stateUpdateService).isWorkingsday(testDate));
 	}
 
+	@Test
+	void time() {
+		final Map<Month, String> expectedValues = new HashMap<>();
+		expectedValues.putAll(Arrays.asList(Month.JANUARY, Month.FEBRUARY, Month.MARCH, Month.NOVEMBER, Month.DECEMBER).stream().collect(Collectors.toMap(month -> month, month -> StateUpdateSeriviceImpl.WINTER)));
+
+		expectedValues.putAll(Arrays.asList(Month.values()).stream().filter(month -> !expectedValues.containsKey(month)).collect(Collectors.toMap(month -> month, month -> StateUpdateSeriviceImpl.SUMMER)));
+
+		Arrays.asList(Month.values()).forEach(month -> {
+			LocalDate date = LocalDate.of(LocalDate.now().getYear(), month, 1);
+			assertEquals(expectedValues.get(month), ((StateUpdateSeriviceImpl) stateUpdateService).time(date), month.name());
+		});
+
+		assertEquals(StateUpdateSeriviceImpl.WINTER, ((StateUpdateSeriviceImpl) stateUpdateService).time(LocalDate.of(2018, 3, 24)));
+		assertEquals(StateUpdateSeriviceImpl.SUMMER, ((StateUpdateSeriviceImpl) stateUpdateService).time(LocalDate.of(2018, 3, 25)));
+		assertEquals(StateUpdateSeriviceImpl.SUMMER, ((StateUpdateSeriviceImpl) stateUpdateService).time(LocalDate.of(2018, 10, 27)));
+		assertEquals(StateUpdateSeriviceImpl.WINTER, ((StateUpdateSeriviceImpl) stateUpdateService).time(LocalDate.of(2018, 10, 28)));
+
+	}
 }
