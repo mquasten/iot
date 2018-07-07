fef3b34543188e0fafe2f0331d52a6fdb744800b
Manfred Quasten <manfred.quasten@arcor.de>
2018-07-07 14:41:34 +0200

    testMain ip

---
+++ iot.batch/src/test/java/de/mq/iot/state/support/TestMain.java
@@ -0,0 +1,28 @@
+package de.mq.iot.state.support;
+
+import java.net.InetAddress;
+import java.util.stream.IntStream;
+
+public class TestMain {
+	
+	public static void main(String[] args)  {
+	
+		IntStream.range(100, 111).forEach(address -> {
+			try {
+				System.out.println(address + ":" +  InetAddress.getByName("192.168.2." + address).getHostName());
+					
+				
+			
+			} catch (Exception e) {
+				System.out.println("sucks:" + ("192.168.2." + address));
+			}
+			
+		
+		});
+		
+	
+	}
+		
+	
+}
+
