a87eb10853e51d7f416f85d6cb1837af5d7c5d37
Manfred Quasten <manfred.quasten@arcor.de>
2018-08-22 20:38:46 +0200

    calendarModel created

--- iot.vaadin.web/src/test/java/de/mq/iot/calendar/support/CalendarModelTest.java
+++ iot.vaadin.web/src/test/java/de/mq/iot/calendar/support/CalendarModelTest.java
@@ -1,5 +1,20 @@
 package de.mq.iot.calendar.support;
 
-public class CalendarModelTest {
 
+import org.mockito.Mockito;
+
+import de.mq.iot.calendar.support.CalendarModel.Events;
+import de.mq.iot.model.Subject;
+
+class CalendarModelTest {
+
+	@SuppressWarnings("unchecked")
+	private final Subject<Events, CalendarModel>   subject = Mockito.mock( Subject.class);
+	
+	private final CalendarModel calendarModel = new CalendarModelImpl(subject);
+	
+	
+	
+	
+	
 }
