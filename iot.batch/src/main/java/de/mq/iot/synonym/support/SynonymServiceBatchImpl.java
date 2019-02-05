9f0709b6e426b7b8eda723efbac844e5058ebf2a
Manfred Quasten <manfred.quasten@arcor.de>
2019-02-05 20:35:35 +0100

    export synonyms

--- iot.batch/src/main/java/de/mq/iot/synonym/support/SynonymServiceBatchImpl.java
+++ iot.batch/src/main/java/de/mq/iot/synonym/support/SynonymServiceBatchImpl.java
@@ -2,16 +2,18 @@ package de.mq.iot.synonym.support;
 
 
 
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.QuoteMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
 import de.mq.iot.state.Command;
 import de.mq.iot.state.Commands;
@@ -40,15 +42,35 @@ public class SynonymServiceBatchImpl implements SynonymServiceBatch {
 	@Commands(commands = {  @Command( name = "exportSynonyms", arguments = {}) })
 	public  void exportSynonyms() throws Exception {
 		final List<Synonym> synonyms = new ArrayList<>(synonymService.deviveSynonyms());
+		try(final BufferedWriter writer = Files.newBufferedWriter(Paths.get("synonyms.csv"));) {
+		write(synonyms, writer);}
+	}
+
+
+
+
+
+	private void write(final List<Synonym> synonyms, final BufferedWriter writer) throws IOException {
+		try(final CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                .withHeader("key", "value", "type", "description").withQuoteMode(QuoteMode.MINIMAL).withDelimiter(';'))) {
 		
-		try ( Writer writer = new FileWriter("synonyms.csv");) {
-	     StatefulBeanToCsv<Synonym> beanToCsv = new StatefulBeanToCsvBuilder<Synonym>(writer).withThrowExceptions(true).build();
-	   
-	    
-	     beanToCsv.write(synonyms);
-	    
+		
+				synonyms.forEach(synonym -> print(csvPrinter,synonym));
 		}
-		System.out.println(synonyms.get(0).key());
+	}
+
+
+
+
+
+	protected void print(final CSVPrinter csvPrinter, final Synonym synonym) {
+		try {
+			csvPrinter.printRecord(synonym.key(), synonym.value(), synonym.type(), synonym.description());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
