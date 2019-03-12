1c491f73848f8a3c71b87439acf5f34bad5eb7dd
Manfred Quasten <manfred.quasten@arcor.de>
2019-03-12 20:55:49 +0100

    BooleanValidator

---
+++ iot.application/src/main/java/de/mq/iot/rule/support/BooleanValidatorImpl.java
@@ -0,0 +1,48 @@
+package de.mq.iot.rule.support;
+
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+class BooleanValidatorImpl implements Validator {
+
+	static final String INVALID = "invalid";
+
+	static final String MANDATORY = "mandatory";
+
+	private ConversionService conversionService = new DefaultConversionService();
+
+	private final boolean mandatory;
+
+	BooleanValidatorImpl(final boolean mandatory) {
+
+		this.mandatory = mandatory;
+	}
+
+	@Override
+	public boolean supports(final Class<?> clazz) {
+		return String.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, final Errors errors) {
+		final String value = (String) target;
+		if ((value == null) && (!mandatory)) {
+			return;
+		}
+
+		if (value == null) {
+			errors.rejectValue(null, MANDATORY);
+			return;
+		}
+		try {
+			conversionService.convert(value, Boolean.class);
+		} catch (final ConversionFailedException ce) {
+			errors.rejectValue(null, INVALID);
+		}
+	}
+
+}
---
+++ iot.application/src/test/java/de/mq/iot/rule/support/BooleanValidatorTest.java
@@ -0,0 +1,76 @@
+package de.mq.iot.rule.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.Validator;
+
+class BooleanValidatorTest {
+
+	private final Validator validator = new BooleanValidatorImpl(true);
+
+	private final Map<String, String> map = new HashMap<>();
+
+	private final Errors errors = new MapBindingResult(map, "ruleInputData");
+
+	private static final String FIELD = "testMode";
+
+	@Test
+	void validate() {
+		validator.validate("false", errors);
+		assertEquals(0, errors.getAllErrors().size());
+		validator.validate("true", errors);
+		assertEquals(0, errors.getAllErrors().size());
+		validator.validate("0", errors);
+		assertEquals(0, errors.getAllErrors().size());
+		validator.validate("1", errors);
+		assertEquals(0, errors.getAllErrors().size());
+	}
+
+	@Test
+	void validateInvalid() {
+		errors.setNestedPath(FIELD);
+		validator.validate("2", errors);
+		assertEquals(1, errors.getAllErrors().size());
+
+		final FieldError fieldError = errors.getFieldErrors().stream().findAny().get();
+
+		assertEquals(BooleanValidatorImpl.INVALID, fieldError.getCode());
+		assertEquals(FIELD, fieldError.getField());
+	}
+
+	@Test
+	void validateInvalidMandatory() {
+		errors.setNestedPath(FIELD);
+		validator.validate(null, errors);
+		assertEquals(1, errors.getAllErrors().size());
+
+		final FieldError fieldError = errors.getFieldErrors().stream().findAny().get();
+
+		assertEquals(BooleanValidatorImpl.MANDATORY, fieldError.getCode());
+		assertEquals(FIELD, fieldError.getField());
+	}
+
+	@Test
+	void validateValidMandatory() {
+
+		final Validator validator = new BooleanValidatorImpl(false);
+		validator.validate(null, errors);
+		assertEquals(0, errors.getAllErrors().size());
+	}
+
+	@Test
+	void supports() {
+		assertTrue(validator.supports(String.class));
+		assertFalse(validator.supports(Boolean.class));
+	}
+
+}
