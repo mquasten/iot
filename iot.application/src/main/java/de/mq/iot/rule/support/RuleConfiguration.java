a5d9b49610677d77e979b934959cf9e09c149e09
Manfred Quasten <manfred.quasten@arcor.de>
2019-03-24 14:00:06 +0100

    RuleConfiguration test

--- iot.application/src/main/java/de/mq/iot/rule/support/RuleConfiguration.java
+++ iot.application/src/main/java/de/mq/iot/rule/support/RuleConfiguration.java
@@ -2,15 +2,20 @@ package de.mq.iot.rule.support;
 
 import java.time.LocalTime;
 
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.support.ConditionalRuleGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
+
+import de.mq.iot.rule.RulesDefinition;
 @Configuration
 class RuleConfiguration {
 	@Bean
 	ConversionService conversionService() {
-	final  DefaultConversionService conversionService= new DefaultConversionService();
+	DefaultConversionService conversionService= new DefaultConversionService();
 			conversionService.addConverter(String.class, LocalTime.class, stringValue -> {
 				String[] values = TimeValidatorImpl.splitTimeString(stringValue);
 				if( values.length!=2) {
@@ -21,8 +26,15 @@ class RuleConfiguration {
 		return conversionService;
 	}
 	
+	@Bean
+	@Scope(scopeName="protoType")
+	RulesAggregate rulesAggregate(final ConversionService conversionService) {
 	
-	
+		final ConditionalRuleGroup group = new ConditionalRuleGroup("ConditionalDefaultDailyIotBatchRuleGroup");
+		group.addRule(new InputDataMappingRuleImpl(conversionService));
+		final Rules rules = new Rules(group);
+		return new SimpleRulesAggregateImpl(RulesDefinition.Id.DefaultDailyIotBatch, rules);
+	}
 	
 	
 
--- iot.application/src/test/java/de/mq/iot/rule/support/RuleConfigurationTest.java
+++ iot.application/src/test/java/de/mq/iot/rule/support/RuleConfigurationTest.java
@@ -2,11 +2,25 @@ package de.mq.iot.rule.support;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.core.RuleProxy;
+import org.jeasy.rules.support.ConditionalRuleGroup;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.StringUtils;
+
+import de.mq.iot.rule.RulesDefinition;
 
 class RuleConfigurationTest {
 	
@@ -24,6 +38,37 @@ class RuleConfigurationTest {
 		
 		
 		assertNull(conversionService.convert(String.format("%s", HOUR), LocalTime.class));
+	}
+	
+	
+	@Test
+	void  rulesAggregate() throws Exception {
+		final ConversionService conversionService = Mockito.mock(ConversionService.class);
+		final RulesAggregate rulesAggregate = ruleConfiguration.rulesAggregate(conversionService);
+		
+		assertEquals(RulesDefinition.Id.DefaultDailyIotBatch, rulesAggregate.id());
+		
+		final Rules rules = (Rules) ReflectionTestUtils.getField(rulesAggregate, "rules");
+		final Collection<Rule> rulesList = new ArrayList<>();
+		rules.forEach(rule -> rulesList.add(rule) );
+		
+		assertEquals(1, rulesList.size());
+		
+		ConditionalRuleGroup group = (ConditionalRuleGroup) rulesList.iterator().next();
+		
+		
+		@SuppressWarnings("unchecked")
+		final Collection<Rule> results =   (Collection<Rule>) ReflectionTestUtils.getField(group, "rules");
+		
+		assertEquals(1, results.size());
+		
+		
+		final List<String> ruleNames = results.stream().map(rule -> RuleProxy.asRule(rule).getName()).collect(Collectors.toList());
+		
+		assertTrue(StringUtils.hasText(InputDataMappingRuleImpl.class.getAnnotation( org.jeasy.rules.annotation.Rule.class).name()));
+		
+		assertTrue(ruleNames.contains(InputDataMappingRuleImpl.class.getAnnotation( org.jeasy.rules.annotation.Rule.class).name()));
+		
 	}
 
 }
