06225f3615154185d47e6385daab0302fa344581
Manfred Quasten <manfred.quasten@arcor.de>
2019-04-03 22:11:00 +0200

    workingday + lastbatchrun

--- iot.application/src/main/java/de/mq/iot/rule/support/SystemVariablesRuleImpl.java
+++ iot.application/src/main/java/de/mq/iot/rule/support/SystemVariablesRuleImpl.java
@@ -1,6 +1,9 @@
 package de.mq.iot.rule.support;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -14,51 +17,62 @@ import de.mq.iot.state.State;
 import de.mq.iot.state.StateService;
 import de.mq.iot.state.support.ItemList;
 
-@Rule(name="calendarRule", priority=2)
+@Rule(name = "calendarRule", priority = 2)
 public class SystemVariablesRuleImpl {
-	
-	
+
 	static final String TEMPERATURE_STATE_NAME = "Temperature";
 	static final String LAST_BATCHRUN_STATE_NAME = "LastBatchrun";
 	static final String MONTH_STATE_NAME = "Month";
 	static final String WORKINGDAY_STATE_NAME = "Workingday";
 	static final String TIME_STATE_NAME = "Time";
-	
+	static final String LAST_BATCHRUN_DATE_FORMAT = "dd.MM.yyyy-HH:mm:ss";
+
 	private final StateService stateService;
-	
+
 	SystemVariablesRuleImpl(StateService stateService) {
 		this.stateService = stateService;
 	}
 
 	@Condition
-	 public void updateSystemVariables(@Fact("calendar") final Calendar calendar, @Fact("states") final Collection<State<?>> results) {
-		 final Map<String, State<?>> states = stateService.states().stream().collect(Collectors.toMap(State::name, state -> state));
-		 
-		 final Consumer<? super State<?>> addState = state -> results.add(state);
-		 if( states.containsKey(TIME_STATE_NAME)) {
-			
-			
-			changedState( states.get(TIME_STATE_NAME), calendar.time()).ifPresent( addState);
-			 
-		 }
-		 if( states.containsKey(MONTH_STATE_NAME)) {
-			 
-			 changedState(states.get(MONTH_STATE_NAME), calendar.month()).ifPresent(addState);
-		 }
+	public void updateSystemVariables(@Fact("calendar") final Calendar calendar, @Fact("states") final Collection<State<?>> results) {
+		final Map<String, State<?>> states = stateService.states().stream().collect(Collectors.toMap(State::name, state -> state));
+		final Consumer<? super State<?>> addState = state -> results.add(state);
+		itemValues(calendar).entrySet().stream().filter(entry -> states.containsKey(entry.getKey())).forEach(entry -> changedState(states.get(entry.getKey()), entry.getValue()).ifPresent(addState));
 		
-		 
-		
-	 }
-
-	private Optional<State<?>> changedState(final State<?> state, final Enum<?>  value ) {
-		 final ItemList itemState = (ItemList) state;
-		if( itemState.hasLabel(value.name())){
-			 return Optional.empty();
-		 }
-		itemState.assign(value.name());
-		return Optional.of((State<?>)itemState);
+		stateValues(calendar).entrySet().stream().filter(entry -> states.containsKey(entry.getKey())).forEach(entry -> changedState(states.get(entry.getKey()), entry.getValue()).ifPresent(addState));
 	}
-	
-	
+
+	private Map<String, Object> stateValues(final Calendar calendar) {
+		final Map<String,Object> stateValues = new HashMap<>();
+		stateValues.put(WORKINGDAY_STATE_NAME, calendar.workingDay());
+		stateValues.put(LAST_BATCHRUN_STATE_NAME, new SimpleDateFormat(LAST_BATCHRUN_DATE_FORMAT).format(new Date(System.currentTimeMillis())));
+		return stateValues;
+	}
+
+	private Map<String, Enum<?>> itemValues(final Calendar calendar) {
+		final Map<String,Enum<?>> itemValues = new HashMap<>();
+		itemValues.put(TIME_STATE_NAME, calendar.time());
+		itemValues.put(MONTH_STATE_NAME, calendar.month());
+		return itemValues;
+	}
+
+	private Optional<State<?>> changedState(final State<?> state, final Enum<?> value) {
+		final ItemList itemState = (ItemList) state;
+		if (itemState.hasLabel(value.name())) {
+			return Optional.empty();
+		}
+		itemState.assign(value.name());
+		return Optional.of((State<?>) itemState);
+	}
+
+	private Optional<State<?>> changedState(final State<?> state, final Object value) {
+		@SuppressWarnings("unchecked")
+		State<Object> simpleState = (State<Object>) state;
+		if (simpleState.hasValue(value)) {
+			return Optional.empty();
+		}
+		simpleState.assign(value);
+		return Optional.of(state);
+	}
 
 }
--- iot.application/src/test/java/de/mq/iot/rule/support/SystemVariablesRuleTest.java
+++ iot.application/src/test/java/de/mq/iot/rule/support/SystemVariablesRuleTest.java
@@ -4,6 +4,8 @@ package de.mq.iot.rule.support;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,10 +13,10 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import de.mq.iot.rule.support.Calendar.Time;
-import de.mq.iot.rule.support.SystemVariablesRuleImpl;
 import de.mq.iot.state.State;
 import de.mq.iot.state.StateService;
 import de.mq.iot.state.support.ItemList;
@@ -32,33 +34,55 @@ class SystemVariablesRuleTest {
 	@SuppressWarnings("unchecked")
 	private final State<Integer> monthState = Mockito.mock(State.class,Mockito.withSettings().extraInterfaces(ItemList.class));
 	
+	
+	@SuppressWarnings("unchecked")
+	private final State<Boolean> workingdayState = Mockito.mock(State.class);
+	
+	@SuppressWarnings("unchecked")
+	private final State<String> lastupdateState = Mockito.mock(State.class);
+	
 	private final List<State<?>> results = new ArrayList<>();
 	
 	@BeforeEach
 	void setup( ) {
-		Mockito.doReturn(Arrays.asList(timeState,monthState)).when(stateService).states();
+		Mockito.doReturn(Arrays.asList(timeState,monthState,workingdayState,lastupdateState)).when(stateService).states();
 		Mockito.doReturn(SystemVariablesRuleImpl.TIME_STATE_NAME).when(timeState).name();
 		
 		Mockito.doReturn(SystemVariablesRuleImpl.MONTH_STATE_NAME).when(monthState).name();
-		
+		Mockito.doReturn(SystemVariablesRuleImpl.WORKINGDAY_STATE_NAME).when(workingdayState).name();
+		Mockito.doReturn(SystemVariablesRuleImpl.LAST_BATCHRUN_STATE_NAME).when(lastupdateState).name();
 	}
 	
 	@Test
-	 void updateSystemVariables() {
+	 void updateSystemVariables() throws ParseException {
 		final Calendar calendar = new Calendar();
 		calendar.assignTime(Time.Summer);
 		final LocalDate now = LocalDate.now();
 		calendar.assignDate(now);
+		calendar.assignWorkingDay(true);
 		 
 		systemVariablesRule.updateSystemVariables(calendar,results);
 		 
-		assertEquals(2, results.size());
+		assertEquals(4, results.size());
 		
 		assertTrue(results.contains(timeState));
+		assertTrue(results.contains(monthState));
+		assertTrue(results.contains(workingdayState));
+		assertTrue(results.contains(lastupdateState));
 		 
 		Mockito.verify(((ItemList)timeState)).assign(Time.Summer.name());
 		
 		Mockito.verify(((ItemList)monthState)).assign(now.getMonth().name());
+		
+		
+		Mockito.verify(workingdayState).assign(true);
+		
+		ArgumentCaptor<String> dateCapture = ArgumentCaptor.forClass(String.class);
+		Mockito.verify(lastupdateState).assign(dateCapture.capture());
+		
+		assertTrue(System.currentTimeMillis() - new SimpleDateFormat(SystemVariablesRuleImpl.LAST_BATCHRUN_DATE_FORMAT).parse(dateCapture.getValue()).getTime() < 1000d);
+		
+
 	 }
 
 }
