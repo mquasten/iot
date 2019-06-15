cb5312bdd9c531483a083ff7db7e027577fd1062
Manfred Quasten <manfred.quasten@arcor.de>
2019-06-15 16:55:30 +0200

    add Editor

--- iot.vaadin.web/src/main/java/de/mq/iot/rule/support/RulesDefinitionView.java
+++ iot.vaadin.web/src/main/java/de/mq/iot/rule/support/RulesDefinitionView.java
@@ -1,16 +1,13 @@
 package de.mq.iot.rule.support;
 
 
-import java.util.Arrays;
+import java.util.Map.Entry;
 
 import org.springframework.context.MessageSource;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.formlayout.FormLayout.FormItem;
-import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.grid.Grid;
+
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -39,26 +36,7 @@ class  RulesDefinitionView extends VerticalLayout implements LocalizeView {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	@I18NKey("name")
-	private final Label nameLabel = new Label(); 
-	private final TextField nameTextField = new TextField();
-	@I18NKey("date")
-	private final Label dateLabel = new Label(); 
-	private final TextField lastUpdateTextField = new TextField();
-	private final TextField valueTextField = new TextField();
-	private final ComboBox<Object> valueComboBox = new ComboBox<>();
-	@I18NKey("reset")
-	private  final Button resetButton = new Button();
-	@I18NKey("save")
-	private  final Button saveButton = new Button();
 	
-	private  final Label stateInfoLabel = new Label();
-	
-	@I18NKey("value")
-	private final Label textValueLabel = new Label();
-	
-	@I18NKey("value")
-	private final Label listValueLabel = new Label();
 	
 	@I18NKey("id_column")
 	private final Label ruleDefinitionColumnLabel = new Label();
@@ -69,15 +47,16 @@ class  RulesDefinitionView extends VerticalLayout implements LocalizeView {
 	
 	@I18NKey("value_column")
 	private final Label valueColumnLabel = new Label();
-	private FormItem textFieldFormItem ; 
-	private FormItem comboBoxFormItem ; 
+
 	private final Grid<RulesDefinition> grid = new Grid<>();
 	
 	
 	private final Grid<String> optionalRules = new Grid<>();
 	
-	private final FormLayout formLayout = new FormLayout();
+	private final Grid<Entry<String,String>> inputParameter = new Grid<>();
 	
+	
+	private final Grid<Entry<String,String>> arguments = new Grid<>();
 	
 //https://vaadin.com/components/vaadin-grid/java-examples/grid-editor
 
@@ -92,7 +71,12 @@ class  RulesDefinitionView extends VerticalLayout implements LocalizeView {
 			
 			System.out.println("selectionChanged:"  + ruleDefinitionModel.selectedRuleDefinition());
 			
-			ruleDefinitionModel.selectedRuleDefinition().ifPresent(rd -> optionalRules.setItems(rd.optionalRules()));
+			ruleDefinitionModel.selectedRuleDefinition().ifPresent(rd -> {
+			optionalRules.setItems(rd.optionalRules());
+			inputParameter.setItems(rd.inputData().entrySet());
+			arguments.setItems(rd.inputData().entrySet());
+			});
+			
 			
 		});
 		
@@ -136,60 +120,62 @@ class  RulesDefinitionView extends VerticalLayout implements LocalizeView {
 	private void createUI(final RulesService ruleService, final ButtonBox buttonBox) {
 				
 	
-		saveButton.setEnabled(false);
-		resetButton.setEnabled(false);
-		
-		valueComboBox.setRequired(true);
-		valueTextField.setRequired(true);
 		
 		
-		final HorizontalLayout layout = new HorizontalLayout(grid,optionalRules);
+	
+		
+		
+		final HorizontalLayout layout = new HorizontalLayout(grid, arguments);
 		grid.getElement().getStyle().set("overflow", "auto");
 		optionalRules.getElement().getStyle().set("overflow", "auto");
-
+		inputParameter.getElement().getStyle().set("overflow", "auto");
 	
-		nameTextField.setSizeFull();
-		nameTextField.setReadOnly(true);
+		
+		
+		arguments.getElement().getStyle().set("overflow", "auto");
+		
+		
+		grid.addColumn((ValueProvider<RulesDefinition, String>) rulesDefinition -> rulesDefinition.id().name()).setHeader(ruleDefinitionColumnLabel).setResizable(true);
+		grid.setSelectionMode(SelectionMode.SINGLE);
+		
+		
+		arguments.addColumn((ValueProvider<Entry<String,String>, String>) entry -> entry.getKey()).setHeader("Parameter").setResizable(true);
+		arguments.addColumn((ValueProvider<Entry<String,String>, String>) entry -> entry.getValue()).setHeader("Wert").setResizable(true);
+		
+		
+		
+		
+		
+		inputParameter.addColumn((ValueProvider<Entry<String,String>, String>) entry -> entry.getKey()).setHeader("Parameter").setFooter(new Button("ändern")).setResizable(true);
+		inputParameter.addColumn((ValueProvider<Entry<String,String>, String>) entry -> entry.getValue()).setHeader("Wert").setFooter(new TextField()).setResizable(true);
+		
+		
+		
 	
-		lastUpdateTextField.setSizeFull();
-		lastUpdateTextField.setReadOnly(true);
 		
-		valueTextField.setSizeFull();
-		valueComboBox.setSizeFull();
-		valueTextField.setReadOnly(true);
-		formLayout.addFormItem(nameTextField, nameLabel);
-		formLayout.addFormItem(lastUpdateTextField, dateLabel);
-		textFieldFormItem=formLayout.addFormItem(valueTextField, textValueLabel);
-		comboBoxFormItem=formLayout.addFormItem(valueComboBox, listValueLabel);
-		comboBoxFormItem.setVisible(false);
+		final HorizontalLayout persistentParameterLayout = new HorizontalLayout(inputParameter, optionalRules);
+		persistentParameterLayout.setSizeFull();
 		
-
-		formLayout.setSizeFull();
-
-		formLayout.setResponsiveSteps(new ResponsiveStep("10vH",1));
-
+		//inputParameterLayout.setSizeFull();
 		
-
-		final VerticalLayout buttonLayout = new VerticalLayout(resetButton, saveButton);
+		final HorizontalLayout editorLayout = new HorizontalLayout(persistentParameterLayout);
 
 		
 		
-		final HorizontalLayout editorLayout = new HorizontalLayout(formLayout, buttonLayout);
-
-		editorLayout.setVerticalComponentAlignment(Alignment.CENTER, buttonLayout);
+		editorLayout.setVerticalComponentAlignment(Alignment.CENTER, persistentParameterLayout);
+		
+		persistentParameterLayout.setVerticalComponentAlignment(Alignment.CENTER, inputParameter);
 
 		editorLayout.setSizeFull();
 		
-		
+	  
 	
-		grid.addColumn((ValueProvider<RulesDefinition, String>) rulesDefinition -> rulesDefinition.id().name()).setHeader(ruleDefinitionColumnLabel).setResizable(true);
-	   
-		grid.setSelectionMode(SelectionMode.SINGLE);
+		
 		
 		optionalRules.addColumn((ValueProvider<String, String>) value -> value).setHeader(optionalRuleColumnLabel).setResizable(true);
 		
-		add(buttonBox, layout, stateInfoLabel, editorLayout);
-		setHorizontalComponentAlignment(Alignment.CENTER, stateInfoLabel);
+		add(buttonBox, layout, editorLayout);
+		setHorizontalComponentAlignment(Alignment.CENTER);
 	
 		
 		layout.setSizeFull();
@@ -197,13 +183,14 @@ class  RulesDefinitionView extends VerticalLayout implements LocalizeView {
 		setHorizontalComponentAlignment(Alignment.CENTER, layout);
 	
 		
-	
-	   grid.setHeight("50vH");
-	 
-	   optionalRules.setHeight("50vH");
-	   
-	   
 		
+	
+	   grid.setHeight("30vH");
+	 
+	   optionalRules.setHeight("30vH");
+	   inputParameter.setHeight("30vH");
+	   
+	   arguments.setHeight("30vH");
 	}
 
 
