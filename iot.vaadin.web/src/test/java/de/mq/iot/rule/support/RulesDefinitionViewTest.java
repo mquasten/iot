b38fc006f6f673297fd0f40759194617570ff830
Manfred Quasten <manfred.quasten@arcor.de>
2019-06-27 19:49:30 +0200

    add tests RulesDefinitionView

---
+++ iot.vaadin.web/src/test/java/de/mq/iot/rule/support/RulesDefinitionViewTest.java
@@ -0,0 +1,113 @@
+package de.mq.iot.rule.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.context.MessageSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.provider.Query;
+
+import de.mq.iot.model.Observer;
+import de.mq.iot.rule.RulesDefinition;
+
+import de.mq.iot.support.ButtonBox;
+
+class RulesDefinitionViewTest {
+
+	private final RuleDefinitionModel ruleDefinitionModel = Mockito.mock(RuleDefinitionModel.class);
+
+	private final RulesService rulesService = Mockito.mock(RulesService.class);
+
+	private final MessageSource messageSource = Mockito.mock(MessageSource.class);
+
+	private RulesDefinitionView rulesDefinitionView;
+
+	private final Map<RuleDefinitionModel.Events, Observer> observers = new HashMap<>();
+
+	private RulesDefinition rulesDefinitionDefaultDailyIotBatch = Mockito.mock(RulesDefinition.class);
+	private RulesDefinition rulesDefinitionEndOfDayBatch = Mockito.mock(RulesDefinition.class);
+
+	private Map<String, Object> fields = new HashMap<>();
+
+	@BeforeEach
+	void setup() {
+
+		Mockito.doReturn(RulesDefinition.Id.DefaultDailyIotBatch).when(rulesDefinitionDefaultDailyIotBatch).id();
+
+		Mockito.doReturn(RulesDefinition.Id.EndOfDayBatch).when(rulesDefinitionEndOfDayBatch).id();
+
+		Mockito.doReturn(Arrays.asList(rulesDefinitionDefaultDailyIotBatch, rulesDefinitionEndOfDayBatch)).when(rulesService).rulesDefinitions();
+
+		Mockito.doAnswer(answer -> {
+
+			final RuleDefinitionModel.Events event = (RuleDefinitionModel.Events) answer.getArguments()[0];
+			final Observer observer = (Observer) answer.getArguments()[1];
+			observers.put(event, observer);
+			return null;
+
+		}).when(ruleDefinitionModel).register(Mockito.any(), Mockito.any());
+
+		rulesDefinitionView = new RulesDefinitionView(ruleDefinitionModel, rulesService, new ButtonBox(), messageSource);
+
+		fields.putAll(Arrays.asList(rulesDefinitionView.getClass().getDeclaredFields()).stream().filter(field -> !Modifier.isStatic(field.getModifiers())).collect(Collectors.toMap(Field::getName, field -> ReflectionTestUtils.getField(rulesDefinitionView, field.getName()))));
+	}
+
+	@Test
+	void init() {
+		assertEquals(RuleDefinitionModel.Events.values().length, observers.size());
+
+		Arrays.asList(RuleDefinitionModel.Events.values()).forEach(key -> observers.containsKey(key));
+
+		@SuppressWarnings("unchecked")
+		final Grid<RulesDefinition> grid = (Grid<RulesDefinition>) fields.get("grid");
+		assertNotNull(grid);
+
+		Collection<RulesDefinition> rulesDefinitions = (Collection<RulesDefinition>) grid.getDataProvider().fetch(new Query<>()).collect(Collectors.toList());
+
+		assertEquals(2, rulesDefinitions.size());
+
+		final Button button = (Button) fields.get("saveButton");
+
+		assertNotNull(button);
+
+		assertFalse(button.isEnabled());
+
+		@SuppressWarnings("unchecked")
+		final Grid<String> optionalRules = (Grid<String>) fields.get("optionalRules");
+		assertNotNull(optionalRules);
+
+		assertFalse(optionalRules.getParent().isPresent());
+
+		@SuppressWarnings("unchecked")
+		final Grid<Entry<String, String>> inputParameter = (Grid<Entry<String, String>>) fields.get("inputParameter");
+
+		assertNotNull(inputParameter);
+		assertFalse(inputParameter.getParent().isPresent());
+
+		@SuppressWarnings("unchecked")
+		final Grid<Entry<String, String>> arguments = (Grid<Entry<String, String>>) fields.get("arguments");
+
+		assertNotNull(arguments);
+		assertFalse(arguments.getParent().isPresent());
+		
+		
+		Mockito.verify(ruleDefinitionModel).notifyObservers(RuleDefinitionModel.Events.ChangeLocale);
+	}
+
+}
