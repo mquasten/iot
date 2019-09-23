b9df42b9fc5e33a5e63c382b253716abb3e7e7ee
Manfred Quasten <manfred.quasten@arcor.de>
2019-09-23 20:21:07 +0200

    add tests

--- iot.application/src/main/java/de/mq/iot/authentication/support/AuthentificationServiceImpl.java
+++ iot.application/src/main/java/de/mq/iot/authentication/support/AuthentificationServiceImpl.java
@@ -12,21 +12,26 @@ import org.springframework.util.ReflectionUtils;
 import de.mq.iot.authentication.Authentication;
 import de.mq.iot.authentication.AuthentificationService;
 import de.mq.iot.authentication.Authority;
+
 @Service
 class AuthentificationServiceImpl implements AuthentificationService {
-	
+
 	private static final String CREDENTIALS = "credentials";
 	static final String CREDENTIALS_FIELD_NAME = CREDENTIALS;
 	private final Duration timeout;
 	private final AuthenticationRepository authenticationRepository;
+
 	AuthentificationServiceImpl(final AuthenticationRepository authenticationRepository, @Value("${mongo.timeout:500}") final Integer timeout) {
-		this.timeout=Duration.ofMillis(timeout);
-		this.authenticationRepository=authenticationRepository;
+		this.timeout = Duration.ofMillis(timeout);
+		this.authenticationRepository = authenticationRepository;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see de.mq.iot.authentication.support.AuthentificationService#authentification(java.lang.String)
+	 * 
+	 * @see
+	 * de.mq.iot.authentication.support.AuthentificationService#authentification
+	 * (java.lang.String)
 	 */
 	@Override
 	public Optional<Authentication> authentification(final String username) {
@@ -35,66 +40,68 @@ class AuthentificationServiceImpl implements AuthentificationService {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see de.mq.iot.authentication.AuthentificationService#authentifications()
 	 */
 	@Override
 	public Collection<Authentication> authentifications() {
 		return authenticationRepository.findAll().collectList().block(timeout);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see de.mq.iot.authentication.AuthentificationService#changePassword(java.lang.String, java.lang.String)
+	 * 
+	 * @see
+	 * de.mq.iot.authentication.AuthentificationService#changePassword(java.lang
+	 * .String, java.lang.String)
 	 */
 	@Override
 	public void changePassword(final String username, final String newPassword) {
 		Assert.hasText(username, "Username is mandatory.");
 		Assert.hasText(newPassword, "New Password is mandatory");
-		final Authentication authentication = this.authentification(username).orElseThrow(() -> new IllegalArgumentException(String.format("User %s not found in database." ,  username)));
-		
+		final Authentication authentication = this.authentification(username).orElseThrow(() -> new IllegalArgumentException(String.format("User %s not found in database.", username)));
+
 		authenticationRepository.save(new UserAuthenticationImpl(authentication.username(), newPassword, authentication.authorities())).block(timeout);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see de.mq.iot.authentication.AuthentificationService#changeAuthorities(java.lang.String, java.util.Collection)
+	 * 
+	 * @see
+	 * de.mq.iot.authentication.AuthentificationService#changeAuthorities(java.
+	 * lang.String, java.util.Collection)
 	 */
 	@Override
-	public boolean changeAuthorities(final String username,  final Collection<Authority>authorities) {
+	public boolean changeAuthorities(final String username, final Collection<Authority> authorities) {
 		Assert.hasText(username, "Username is mandatory.");
-		
-		final Authentication authentication = this.authentification(username).orElseThrow(() -> new IllegalArgumentException(String.format("User %s not found in database." ,  username)));
-		
-		
-		if(!modifyUsersRoleCanBeRemoved(authentication, authorities)) {
+
+		final Authentication authentication = this.authentification(username).orElseThrow(() -> new IllegalArgumentException(String.format("User %s not found in database.", username)));
+
+		if (!modifyUsersRoleCanBeRemoved(authentication, authorities)) {
 			return false;
 		}
-		
-	
-		final Authentication changedAAuthentication =  new UserAuthenticationImpl(authentication.username(), "?" ,  authorities);
-		
+
+		final Authentication changedAAuthentication = new UserAuthenticationImpl(authentication.username(), "?", authorities);
+
 		copyPassword(authentication, changedAAuthentication);
-		
+
 		authenticationRepository.save(changedAAuthentication).block(timeout);
-		
-		
-		
+
 		return true;
-		
+
 	}
 
 	private void copyPassword(final Authentication authentication, final Authentication changedAAuthentication) {
 		ReflectionUtils.doWithFields(UserAuthenticationImpl.class, field -> {
-			field.setAccessible(true);	
+			field.setAccessible(true);
 			field.set(changedAAuthentication, field.get(authentication));
-			
+
 		}, field -> field.getName().equalsIgnoreCase(CREDENTIALS));
 	}
 
 	private boolean modifyUsersRoleCanBeRemoved(final Authentication authentication, final Collection<Authority> authorities) {
-		if ( authentication.hasRole(Authority.ModifyUsers) && ! authorities.contains(Authority.ModifyUsers)) {
-			
-			
+		if (authentication.hasRole(Authority.ModifyUsers) && !authorities.contains(Authority.ModifyUsers)) {
+
 			return Optional.ofNullable(authenticationRepository.findByUsernameNotAndAuthority(authentication.username(), Authority.ModifyUsers).blockFirst(timeout)).isPresent();
 		}
 		return true;
--- iot.application/src/test/java/de/mq/iot/authentication/support/AuthentificationServiceTest.java
+++ iot.application/src/test/java/de/mq/iot/authentication/support/AuthentificationServiceTest.java
@@ -1,6 +1,7 @@
 package de.mq.iot.authentication.support;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,7 +20,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class AuthentificationServiceTest {
-	
+
 	private static final String NEW_PASSWORD = "fever";
 
 	private static final String USER = "kminogue";
@@ -27,96 +28,154 @@ public class AuthentificationServiceTest {
 	private static final int TIMEOUT = 500;
 
 	private AuthenticationRepository authenticationRepository = Mockito.mock(AuthenticationRepository.class);
-	
-	private AuthentificationServiceImpl  authentificationService = new AuthentificationServiceImpl(authenticationRepository, TIMEOUT);
-	
+
+	private AuthentificationServiceImpl authentificationService = new AuthentificationServiceImpl(authenticationRepository, TIMEOUT);
+
 	private Authentication authentication = Mockito.mock(Authentication.class);
-	
+
 	@SuppressWarnings("unchecked")
 	private Mono<Authentication> mono = Mockito.mock(Mono.class);
-	
+
 	@Test
 	void authentification() {
-		
+
 		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
 		Mockito.doReturn(authentication).when(mono).block(Duration.ofMillis(TIMEOUT));
 		assertEquals(Optional.of(authentication), authentificationService.authentification(USER));
 	}
-	
-	
+
 	@Test
 	void authentifications() {
 		Mockito.doReturn(Flux.just(authentication)).when(authenticationRepository).findAll();
 		final Collection<Authentication> authentications = authentificationService.authentifications();
-		
+
 		assertEquals(1, authentications.size());
 		assertEquals(authentication, authentications.stream().findAny().get());
 	}
-	
+
 	@Test
 	void changePassword() {
-		
+
 		@SuppressWarnings("unchecked")
-		final  Mono<Authentication> saveMono = Mockito.mock(Mono.class);
+		final Mono<Authentication> saveMono = Mockito.mock(Mono.class);
 		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
 		Mockito.doReturn(USER).when(authentication).username();
 		Mockito.doReturn(authentication).when(mono).block(Duration.ofMillis(TIMEOUT));
 		Mockito.doReturn(saveMono).when(authenticationRepository).save(Mockito.any(Authentication.class));
-		
+
 		authentificationService.changePassword(USER, NEW_PASSWORD);
-		
+
 		final ArgumentCaptor<Authentication> usercaptor = ArgumentCaptor.forClass(Authentication.class);
-		
+
 		Mockito.verify(authenticationRepository).save(usercaptor.capture());
 		Mockito.verify(saveMono).block(Duration.ofMillis(TIMEOUT));
 		assertEquals(USER, usercaptor.getValue().username());
 		assertTrue(usercaptor.getValue().authenticate(NEW_PASSWORD));
-		
+
 	}
-	
+
 	@Test
 	void changePasswordUserNotFound() {
 		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
-		
+
 		Mockito.doReturn(null).when(mono).block(Duration.ofMillis(TIMEOUT));
-		
+
 		assertThrows(IllegalArgumentException.class, () -> authentificationService.changePassword(USER, NEW_PASSWORD));
 	}
-	
+
 	@Test
 	void changeAuthorities() {
-		
-		Authentication user = new UserAuthenticationImpl(USER, NEW_PASSWORD, Arrays.asList(Authority.ModifyUsers));
-		
-		@SuppressWarnings("unchecked")
-		final  Mono<Authentication> saveMono = Mockito.mock(Mono.class);
-		
-		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
-		
-		
-		//Mockito.doReturn(Boolean.TRUE).when(user).hasRole(Authority.ModifyUsers);
-		Mockito.doReturn(user).when(mono).block(Duration.ofMillis(TIMEOUT));
-		
-		Mockito.doReturn(saveMono).when(authenticationRepository).save(Mockito.any(Authentication.class));
-		
-		
-		final Flux<Authentication> flux = Flux.just(Mockito.mock(Authentication.class));
-		Mockito.doReturn(flux).when(authenticationRepository).findByUsernameNotAndAuthority(USER, Authority.ModifyUsers);
-		
-		
+
+		preparechangeAuthorities(Authority.ModifyUsers);
+
 		assertTrue(authentificationService.changeAuthorities(USER, Arrays.asList(Authority.ModifySystemvariables)));
-		
+
 		ArgumentCaptor<Authentication> authenticationCaptor = ArgumentCaptor.forClass(Authentication.class);
-		
+
 		Mockito.verify(authenticationRepository).save(authenticationCaptor.capture());
-		
-		
+
 		assertEquals(USER, authenticationCaptor.getValue().username());
-		assertTrue(authenticationCaptor.getValue().authenticate("fever"));
+		assertTrue(authenticationCaptor.getValue().authenticate(NEW_PASSWORD));
 		assertEquals(1, authenticationCaptor.getValue().authorities().size());
 		assertEquals(Authority.ModifySystemvariables, authenticationCaptor.getValue().authorities().stream().findAny().get());
-		
-		
+
+	}
+
+	private void preparechangeAuthorities(final Authority existingAuthority) {
+		final Authentication user = new UserAuthenticationImpl(USER, NEW_PASSWORD, Arrays.asList(existingAuthority));
+
+		@SuppressWarnings("unchecked")
+		final Mono<Authentication> saveMono = Mockito.mock(Mono.class);
+
+		Mockito.doReturn(mono).when(authenticationRepository).findByUsername(USER);
+
+		// Mockito.doReturn(Boolean.TRUE).when(user).hasRole(Authority.ModifyUsers);
+		Mockito.doReturn(user).when(mono).block(Duration.ofMillis(TIMEOUT));
+
+		Mockito.doReturn(saveMono).when(authenticationRepository).save(Mockito.any(Authentication.class));
+
+		final Flux<Authentication> flux = Flux.just(Mockito.mock(Authentication.class));
+		Mockito.doReturn(flux).when(authenticationRepository).findByUsernameNotAndAuthority(USER, Authority.ModifyUsers);
+	}
+
+	@Test
+	void changeAuthoritiesLastUserWithoutAdmin() {
+		preparechangeAuthorities(Authority.ModifyUsers);
+
+		final Flux<Authentication> flux = Flux.just();
+		Mockito.doReturn(flux).when(authenticationRepository).findByUsernameNotAndAuthority(USER, Authority.ModifyUsers);
+
+		assertFalse(authentificationService.changeAuthorities(USER, Arrays.asList(Authority.ModifySystemvariables)));
+
+		final ArgumentCaptor<Authentication> authenticationCaptor = ArgumentCaptor.forClass(Authentication.class);
+
+		Mockito.verify(authenticationRepository, Mockito.never()).save(authenticationCaptor.capture());
+
+	}
+
+	@Test
+	void changeAuthoritiesUserNotFound() {
+
+		preparechangeAuthorities(Authority.ModifyUsers);
+
+		Mockito.doReturn(Mono.empty()).when(authenticationRepository).findByUsername(USER);
+
+		assertThrows(IllegalArgumentException.class, () -> authentificationService.changeAuthorities(USER, Arrays.asList(Authority.ModifySystemvariables)));
+
+	}
+
+	@Test
+	void changeAuthoritiesNotAdmin() {
+		preparechangeAuthorities(Authority.ModifySystemvariables);
+
+		assertTrue(authentificationService.changeAuthorities(USER, Arrays.asList(Authority.ModifySystemvariables)));
+
+		final ArgumentCaptor<Authentication> authenticationCaptor = ArgumentCaptor.forClass(Authentication.class);
+
+		Mockito.verify(authenticationRepository).save(authenticationCaptor.capture());
+
+		assertEquals(USER, authenticationCaptor.getValue().username());
+		assertTrue(authenticationCaptor.getValue().authenticate(NEW_PASSWORD));
+		assertEquals(1, authenticationCaptor.getValue().authorities().size());
+		assertEquals(Authority.ModifySystemvariables, authenticationCaptor.getValue().authorities().stream().findAny().get());
+
+	}
+
+	@Test
+	void changeAuthoritiesAdmin() {
+		preparechangeAuthorities(Authority.ModifyUsers);
+
+		assertTrue(authentificationService.changeAuthorities(USER, Arrays.asList(Authority.ModifyUsers)));
+
+		final ArgumentCaptor<Authentication> authenticationCaptor = ArgumentCaptor.forClass(Authentication.class);
+
+		Mockito.verify(authenticationRepository).save(authenticationCaptor.capture());
+
+		assertEquals(USER, authenticationCaptor.getValue().username());
+		assertTrue(authenticationCaptor.getValue().authenticate(NEW_PASSWORD));
+		assertEquals(1, authenticationCaptor.getValue().authorities().size());
+		assertEquals(Authority.ModifyUsers, authenticationCaptor.getValue().authorities().stream().findAny().get());
+
 	}
 
 }
