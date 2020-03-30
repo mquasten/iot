a398af76dada3dda8041e876f0f612886ef5e6fe
Manfred Quasten <manfred.quasten@arcor.de>
2020-03-30 20:24:35 +0200

    add VacationOrPublicHolidayRule

---
+++ iot.application/src/main/java/de/mq/iot/calendar/support/VacationOrHolidayRuleImpl.java
@@ -0,0 +1,37 @@
+package de.mq.iot.calendar.support;
+
+import java.time.LocalDate;
+import java.util.AbstractMap;
+import java.util.Collection;
+
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.AbstractMap.SimpleImmutableEntry;
+
+import de.mq.iot.calendar.Specialday;
+import de.mq.iot.calendar.SpecialdayService.DayType;
+
+
+public class VacationOrHolidayRuleImpl extends AbstractSpecialdaysRule {
+
+	private final Collection<Specialday.Type> types = Set.of( Specialday.Type.Fix,  Specialday.Type.Gauss ,  Specialday.Type.Vacation);
+	
+	VacationOrHolidayRuleImpl() {
+		super(2);
+	}
+
+	
+	
+	@Override
+	Optional<Entry<DayType, String>> execute(final Collection<Specialday> specialday, final LocalDate date) {
+		return   specialday.stream().filter(day -> types.contains(day.type())).filter(day -> day.date(date.getYear()).equals(date))
+				.map(day -> (Entry<DayType, String>) entry(date)).findAny();
+			
+	}
+	
+	private SimpleImmutableEntry<DayType, String> entry(final LocalDate date) {
+		return new AbstractMap.SimpleImmutableEntry<DayType, String>(DayType.SpecialWorkingDay, String.format(DAY_TYPE_INFO_FORMAT, "Vaction or public holiday" , date));
+	}
+
+}
